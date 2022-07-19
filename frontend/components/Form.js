8abbeb544a55cca65c5f8fbf7de3a0cd6b3dd545
Allesandria <mufasabugatti@Mufasas-MBP.lan>
2022-07-19 13:49:46 -0400

    handle change done

--- frontend/components/Form.js
+++ frontend/components/Form.js
@@ -1,6 +1,22 @@
 import React from 'react'
 
 export default class Form extends React.Component {
+  constructor(){
+    super()
+    this.state = {
+      input: ''
+    }
+  }
+
+handleChange = e => {
+  this.setState({
+    ...this.state, input: e.target.value
+  })
+}
+
+handleSubmit
+
+
   render() {
     return (
       <div>
